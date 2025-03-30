9673e4c6dbf7590c82b0e236415ee58fdca1eabc
MohdOmarFarhan <mohd.omarfarhan@gmail.com>
2025-03-30 20:08:56 +0600

    homepage added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 1. Project setup complete
+2. Hmepage creation:
+    i. HomeController create
---
+++ resources/js/Pages/Home/HomePage.vue
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<template>
+    <h1 class="text-green-500">This is homepage</h1>
+</template>
+
+<style>
+
+</style>
