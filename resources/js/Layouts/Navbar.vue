0a14b3b4e8fc1b2b8966f8a3d6fd0adba95ba1f3
prismtechbd3 <prismtechb@gmail.com>
2025-04-01 13:48:32 +0600

    conitnue

---
+++ resources/js/Layouts/Navbar.vue
@@ -0,0 +1,13 @@
+
+<script setup>
+
+</script>
+
+<template>
+  <h1> This is Navbar </h1>
+</template>
+
+
+<style>
+
+</style>
--- resources/js/Pages/Admin/AdminDashboard.vue
+++ resources/js/Pages/Admin/AdminDashboard.vue
@@ -1,9 +1,11 @@
-<script>
-
+<script setup>
+import AdminLayout from '@/Layouts/AdminLayout.vue';
 </script>
 
 <template>
+    <AdminLayout> 
     <h1 class="text-green-500">This is Admin Dashboard</h1>
+    </AdminLayout>
 </template>
 
 <style>
